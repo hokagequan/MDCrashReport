69cbfd4f2e98d904dde72a1886d126e75f72a1c4
权成文 <quanchengwen@zhizhangyi.com>
2019-03-04 10:36:36 +0800

    base

---
+++ MDCrashReport/MDCrashReport/MDCrashReport.h
@@ -0,0 +1,19 @@
+//
+//  MDCrashReport.h
+//  MDCrashReport
+//
+//  Created by QiM on 2019/3/4.
+//  Copyright © 2019 QiM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MDCrashReport.
+FOUNDATION_EXPORT double MDCrashReportVersionNumber;
+
+//! Project version string for MDCrashReport.
+FOUNDATION_EXPORT const unsigned char MDCrashReportVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MDCrashReport/PublicHeader.h>
+
+
